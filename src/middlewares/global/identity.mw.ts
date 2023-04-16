854cf7e6ea3327320e488f82b08eb4bf3e75afa5
Duc Filan <ducfilan@gmail.com>
2023-04-16 16:15:29 +0800

    Add cache to identity middleware

--- src/middlewares/global/identity.mw.ts
+++ src/middlewares/global/identity.mw.ts
@@ -1,6 +1,8 @@
 import UsersDao from '@dao/users.dao'
 import { getEmailFromGoogleToken } from '@services/support/google-auth.service'
-import { LoginTypes } from '@common/consts'
+import { CacheKeyUser, LoginTypes } from '@common/consts'
+import { getCache, setCache } from '@/common/redis'
+import { ObjectId } from 'mongodb'
 
 export default async (req, res, next) => {
   try {
@@ -20,7 +22,15 @@ export default async (req, res, next) => {
 
     if (!email) return next()
 
-    const user = await UsersDao.findByEmail(email)
+    const cacheKey = CacheKeyUser(email)
+    let user = await getCache(cacheKey)
+
+    if (user) {
+      user._id = new ObjectId(user._id)
+    } else {
+      user = await UsersDao.findByEmail(email)
+      setCache(cacheKey, user)
+    }
 
     if (!user) return next()
 
