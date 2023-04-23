baae72df5d176275e9471562a7d43e0445284b0e
Duc Filan <ducfilan@gmail.com>
2023-04-23 10:00:18 +0800

    Fix participant in conversation check

--- src/services/api/conversations.services.ts
+++ src/services/api/conversations.services.ts
@@ -3,5 +3,5 @@ import { ObjectId } from "mongodb"
 
 export async function isParticipantInConversation(userId: ObjectId, conversationId: ObjectId): Promise<boolean> {
   const conversation = await ConversationsDao.findOne(conversationId)
-  return conversation.participants.some(((p) => p._id === userId))
+  return conversation.participants.some(((p) => p._id.equals(userId)))
 }
