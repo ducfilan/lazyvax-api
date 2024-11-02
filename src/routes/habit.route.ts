dd4756f59ad32e43df9d17db50b2c62d8b79819f
Duc Filan <ducfilan@gmail.com>
2024-11-02 16:36:52 +0800

    Habit API routing

--- src/routes/habit.route.ts
+++ src/routes/habit.route.ts
@@ -2,14 +2,14 @@ import { Router } from 'express';
 import HabitsController from '@controllers/habits.controller';
 import auth from '@middlewares/global/auth.mw';
 
-const habitsRouter = Router();
+const securedHabitsRouter = Router();
 
-habitsRouter.route('/')
+securedHabitsRouter.route('/')
   .get(auth, HabitsController.getHabits)
   .post(auth, HabitsController.createHabit);
 
-habitsRouter.route('/:id')
+securedHabitsRouter.route('/:id')
   .put(auth, HabitsController.updateHabit)
   .delete(auth, HabitsController.deleteHabit);
 
-export default habitsRouter;
+export default securedHabitsRouter;
--- src/routes/secured.index.ts
+++ src/routes/secured.index.ts
@@ -4,6 +4,7 @@ import { securedConversationRouter } from './conversation.route'
 import { securedAiRouter } from './ai.route'
 import { securedEventRouter } from './event.route'
 import { securedObjectiveRouter } from './objective.route'
+import securedHabitsRouter from './habit.route'
 
 let securedRouter = Router()
 
@@ -12,5 +13,6 @@ securedRouter.use('/conversations', securedConversationRouter)
 securedRouter.use('/ai', securedAiRouter)
 securedRouter.use('/events', securedEventRouter)
 securedRouter.use('/objectives', securedObjectiveRouter)
+securedRouter.use('/habits', securedHabitsRouter)
 
 export default securedRouter
