7034265a9f7916436ce235152d334659cb565643
Duc Filan <ducfilan@gmail.com>
2024-12-17 08:40:14 +0800

    Update module aliases in package.json to point to build directory; refactor planning.controller.ts to use getDaySuggestions function
    - Changed module aliases in package.json from 'src' to 'build' to reflect the updated project structure.
    - Refactored planning.controller.ts to import and utilize the getDaySuggestions function directly from planning.services, improving code clarity and maintainability.
    
    These changes streamline the project's structure and enhance the functionality of the planning controller.

--- src/controllers/planning.controller.ts
+++ src/controllers/planning.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import planningService from '@services/api/planning.services';
+import { getDaySuggestions } from '@services/api/planning.services';
 import logger from '@/common/logger';
 import { User } from '@/entities/User';
 import { ObjectId } from 'mongodb';
@@ -11,7 +11,7 @@ export default class PlanningController {
       const date = req.body.date as any as Date;
       const extraInfo = req.body.extraInfo;
 
-      const suggestions = await planningService.getDaySuggestions(req.user, conversationId, date, extraInfo);
+      const suggestions = await getDaySuggestions(req.user, conversationId, date, extraInfo);
       res.status(200).json(suggestions);
     } catch (error) {
       logger.error(`Error in runDaySuggestions: ${error}`);
